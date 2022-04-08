c58327a96aee1ad831c0604b70e82cd6cd2d7537
Thomas Källman <thomas.kallman@nrm.se>
2022-04-08 11:30:55 +0200

    update

--- R/createGenoType.R
+++ R/createGenoType.R
@@ -1,3 +1,7 @@
+#' @title 
+#' Extract most likely genotype data from replicates
+#'
+#' @description 
 #' Function to extract the most likely genotype scores from multiple
 #' replicates of genotype data. The structure of the input data is a
 #' table (excel etc) where the first column contains sample names and
@@ -10,36 +14,40 @@
 #' there are spaces in the names. Hence the need to modify the names
 #' with the replacement of "..." with ".".
 #'
+#' 
 #' @return a list of three dataframes: Genotypes, QC and number of
 #' missing data points
 #' @param file name of the file with replicate data
 #' @param locus names of the loci to be analysed
+#' @export
 
 createGT <- function(file = file, locus = c("Locus1", "Locus2")) {
-    platta <- load_data(file)
+    platta <- LoadData(file)
     if(!is.null(platta)) {
-        platta$names <- extract_names(platta[,1])
+        platta$names <- ExtractNames(platta[,1])
         togrep <- paste(locus,collapse="|")
         platta1 <- platta[,grepl(togrep, names(platta))]
         
-        indDataMax <- aggregate(platta1, by = list(platta$names),
-                                max_mod)
+        indDataMax <- stats::aggregate(platta1, by = list(platta$names),
+                                MaxMod)
         row.names(indDataMax) <- indDataMax$Group.1
+        indDataMax <- indDataMax[, - which(names(indDataMax) == "Group.1")]
         colnames(indDataMax) <- paste0(gsub(pattern = "...",
                                             x = colnames(indDataMax),
                                             replacement = ".",
                                             fixed = TRUE),
                                        "max")
-        indDataMin <- aggregate(platta1, by = list(platta$names),
-                                min_mod)
+        indDataMin <- stats::aggregate(platta1, by = list(platta$names),
+                                MinMod)
         row.names(indDataMin) <- indDataMin$Group.1
+        indDataMin <- indDataMin[, - which(names(indDataMin) == "Group.1")]
         colnames(indDataMin) <- paste0(gsub(pattern = "...",
                                             x = colnames(indDataMin),
                                             replacement = ".",
                                             fixed = TRUE),
                                        "min")
         indData <- cbind(indDataMin, indDataMax)
-        QC <- lapply(locus, qc_rep, data = indData)
+        QC <- lapply(locus, QCRep, data = indData)
         QC <- as.data.frame(do.call(cbind, QC))
         colnames(QC) <- paste(locus, ".QC", sep = "")
         indData <- indData[,grepl(".1min|.2max", names(indData))]
@@ -48,7 +56,7 @@ createGT <- function(file = file, locus = c("Locus1", "Locus2")) {
         gt <- QC_Gt[,!grepl("QC", names(QC_Gt))]
         res <- list(QC = QC_Gt,
                     Genotypes = gt,
-                    gt.sum = rowSums(is.na(gt)))
+                    gt.missing = rowSums(is.na(gt)))
         return(res)
     } else {
         cat("")
@@ -56,5 +64,78 @@ createGT <- function(file = file, locus = c("Locus1", "Locus2")) {
     
 }
 
+#' Determine sex from genotype data
+#' 
+#' This function will based on the selected number of loci determine the 
+#' most likely sex of a sample from genotype data. 
+#' NB! The function assumes that the marker with name Male have the following 
+#' properties:
+#' 1. 152 152 == Female
+#' 2.  98 152 == Male
+#' 3.  98  98 == Male
+#' 4.  NA 152 == Female
+#' 5.  NA  98 == Male
+#' 6.  NA  NA == NA
+#' 
+#' The other markers that are y-chromosome markers have the following properties:
+#' 1. NA NA == Female
+#' 2. Any other value == Male
+#' 
+#' Consensus for this is the extracted as follows:
+#' 1. All marker same sex this is the true sex of the sample
+#' 2. All markers have NA, the sex is not determined NA is reported
+#' 3. In case of mismatch the most common sex i extracted.
+#' 4. The Male marker is better as the y-chromosome markers can not separate failed from female.
+#' @param data a dataframe with genotype results
+#' @return list with input data plus QC score and a vector with most likely sex
+sexdetermination <- function(data = data, locus = c("Male", "UaY15020", "UarY369.4")) {
+    genSexOrg <- rep("NA", nrow(data))
+    genSexUay <- rep("NA", nrow(data))
+    genSexUar <- rep("NA", nrow(data))
+    if("Male" %in% locus) {
+        ResOrg <- data[,grepl("Male", names(data))]
+        ResOrg$merge <- paste(ResOrg[,1], ResOrg[,2])
+        genSexOrg <- ifelse(ResOrg$merge == "NA 152"  |
+                                ResOrg$merge == "152 152" |
+                                ResOrg$merge == "152 NA",
+                            yes = "Hona",
+                            no = ifelse(ResOrg$merge == "98 152" |
+                                            ResOrg$merge == "98 NA"  |
+                                            ResOrg$merge == "NA 98"  |
+                                            ResOrg$merge == "152 98",
+                                        yes = "Hane",
+                                        no = NA))
+        
+    }
+    if("UaY15020" %in% locus) {
+        ResUay <- data[,grepl("UaY", names(data))]
+        genSexUay <- ifelse(is.na(ResUay[,1]),
+                            yes = "Hona",
+                            no = "Hane")
+    }
     
+    if("UarY369.4" %in% locus) {
+        ResUar <- data[,grepl("Uar", names(data))]
+        genSexUar <- ifelse(is.na(ResUar[,1]),
+                            yes = "Hona",
+                            no = "Hane")
+    }
+    genSex <- data.frame(genSexOrg, genSexUar, genSexUay)
+    sexQC <- function(vec) {
+        all(vec == vec[1])
+    }
+    QC <- unlist(apply(genSex, MARGIN = 1, sexQC))
+    consensusExtract <- function(vec) {
+        names(sort(table(factor(vec)), decreasing = TRUE))[1]
+    }
+    consensus <- unlist(apply(genSex, MARGIN = 1, consensusExtract))
+    
+    genSex <- cbind(genSex, QC, consensus)
+    colnames(genSex) <- c(names(genSex[-ncol(genSex)]), "Sex")
+    rownames(genSex) <- rownames(data)
+    consensus <- as.data.frame(consensus)
+    rownames(consensus) <- rownames(data)
+    return(list(QC = genSex,
+                Genotypes = consensus))
+}
     
--- R/utils.R
+++ R/utils.R
@@ -4,37 +4,37 @@
 #' @return a dataframe of the imported data
 #' @param file_path name of file to import data from (with path)
 #' @param sheet which sheet to read data from in excel and ods file formats
-#' @param na_strings entries should be converted to NA values on import
+#' @param naStrings entries should be converted to NA values on import
 #' __NB! The function uses file endings to guess file formats. Will not work if file ending__
 #' __is not consistent with actual file type__
 #' @export
-load_data <- function(file_path, sheet = 1, na_strings = c("NA", "-99", "0", "000", "No peaks in locus", "No peaks")) {
-    if (endsWith(file_path, ".xls") | endsWith(file_path, ".xlsx")) {
-        raw_data <- readxl::read_excel(path = file_path,
+LoadData <- function(filePath, sheet = 1, naStrings = c("NA", "-99", "0", "000", "No peaks in locus", "No peaks")) {
+    if (endsWith(filePath, ".xls") | endsWith(filePath, ".xlsx")) {
+        raw_data <- readxl::read_excel(path = filePath,
                                        col_names = TRUE,
-                                       na = na_strings,
+                                       na = naStrings,
                                        sheet = sheet)
-    } else if (endsWith(file_path, ".ods")) {
-        raw_data <- readODS::read_ods(path = file_path,
+    } else if (endsWith(filePath, ".ods")) {
+        raw_data <- readODS::read_ods(path = filePath,
                                       col_names = TRUE,
-                                      na = na_strings,
+                                      na = naStrings,
                                       sheet = sheet)
-    } else if (endsWith(file_path, ".tsv") |
-               endsWith(file_path, ".tdf") |
-               endsWith(file_path, ".txt")) {
-        raw_data <- read.table(file = file_path,
+    } else if (endsWith(filePath, ".tsv") |
+               endsWith(filePath, ".tdf") |
+               endsWith(filePath, ".txt")) {
+        raw_data <- utils::read.table(file = filePath,
                                header = TRUE,
-                               na.strings = na_strings,
+                               na.strings = naStrings,
                                sep = "\t",
                                stringsAsFactors = FALSE)
     } else {
-        raw_data <- read.table(file = file_path,
+        raw_data <- utils::read.table(file = filePath,
                                header = TRUE,
-                               na.strings = na_strings,
+                               na.strings = naStrings,
                                sep = ",",
                                stringsAsFactors = FALSE)
     }
-    raw_datanona <- na.omit(raw_data)
+    raw_datanona <- stats::na.omit(raw_data)
     temp <- any(raw_datanona == "Unbinned peaks in locus" |
                 raw_datanona == "Too many alleles")
     if (temp) {
@@ -51,22 +51,22 @@ load_data <- function(file_path, sheet = 1, na_strings = c("NA", "-99", "0", "00
 #'
 #' @return a vector of names that 10 long if SEP or 7 long in all
 #' other cases
-#' @param name_vector a vector containing entries with names that have
+#' @param nameVector a vector containing entries with names that have
 #' replicate name and SSR mix attached. The latter will be stripped in
 #' and only the _actual_ names of samples will be retained.
 #' 
-extract_names <- function(name_vector) {
-    ifelse(startsWith(name_vector, "SEP"),
-           yes = substring(name_vector, 1, 10),
-           no = substring(name_vector, 1, 7))
+ExtractNames <- function(nameVector) {
+    ifelse(startsWith(nameVector, "SEP"),
+           yes = substring(nameVector, 1, 10),
+           no = substring(nameVector, 1, 7))
 }
 
 #' Check if allele sizes from replicate runs are the same. Returns 'OK'
 #' if all looks identical and returns 'Check' if replicates are
 #' inconsistent. Returns "No data" if there is no data available.
-#' @param dataframe with genotyping results
-#' @locus Name of the marker to analysed
-qc_rep <- function(data, locus = "") {
+#' @param data with genotyping results
+#' @param locus of the marker to analysed
+QCRep <- function(data = data, locus = "") {
   data <- data[,grepl(locus, names(data))]
   QC <- data[,1] == data[,3] & data[,2] == data[,4]
   QC <- ifelse(QC, yes = "OK", no = "Check")
@@ -80,30 +80,115 @@ qc_rep <- function(data, locus = "") {
 #' @param data1 First dataframe with results
 #' @param data2 Second dataframe with results
 #' @export
-mergePlate <- function(GenoMix1, GenoMix2) {
-    if (!identical(row.names(GenoMix1$Genotypes), row.names(GenoMix2$Genotypes))) {
+MergePlate <- function(data1, data2) {
+    if (!identical(row.names(data1$Genotypes), row.names(data2$Genotypes))) {
         cat("Can not merge objects with different names\n")
     } else {
         cat("Data merged\n")
-        return(cbind(GenoMix1$Genotypes, GenoMix2$Genotypes))
+        return(cbind(data1, data2))
     }
 }
 
-#' Extract the max value from a range of values. Returns NA if all
-#' data is NA without generating a warning.
+#' Add genotype results different marker types to 
+#' a single dataframe
+#' @param data1 First dataframe with results
+#' @param data2 Second dataframe with results
+#' @export
+MergePlateSum <- function(data1, data2) {
+  if (!identical(row.names(data1), row.names(data2))) {
+    cat("Can not merge objects with different names\n")
+  } else {
+    cat("Data merged\n")
+    return(cbind(data1, data2))
+  }
+}
+
+#' Extract the max value from a range of values. Returns NA without a
+#' warning if all data is NA.
 #' @param x vector with values
-max_mod <- function(x) {
+#' @return the maximum value from a vector or NA if only missing data
+MaxMod <- function(x) {
     ifelse(!all(is.na(x)),
            max(x, na.rm=T), NA)
 }
 
-#' Extract the min value from a range of values. Returns NA if all
-#' data is NA without generating a warning.
+#' Extract the min value from a range of values. Returns NA without a
+#' warning if all data is NA.
 #' @param x vector with values
-min_mod <- function(x) {
-    ifelse(!all(is.na(x))
-          ,
+#' @return the minimum value of a vector or NA if only missing data
+MinMod <- function(x) {
+    ifelse(!all(is.na(x)),
            min(x, na.rm = T), NA)
 }
 
+#' Merge genotype data with metadata
+#'
+#' 
 
+CreateMatchInput <- function(GenotypeResults, filt = "No") {
+  header <- c("index", "date", "north", "east",
+              "gender", "confirmed_dead", "G10L_1",
+              "G10L_2", "MU05_1", "MU05_2", "MU09_1",
+              "MU09_2", "MU10_1", "MU10_2", "MU23_1",
+              "MU23_2", "MU50_1", "MU50_2", "MU51_1",
+              "MU51_2", "MU59_1", "MU59_2", "Sex_1", "Sex_2")
+  # metadata <- read_excel("~/ownCloud/Spillning2020/QCResults/rovbasedata.xlsx")
+  metadata <- read_excel("~/ownCloud/Spillning2021/rovbasemeta.xlsx")
+  metadata <- metadata[,c(1,4:6)]
+  names(metadata) <- c("index", "north", "east", "date")
+  tt <- merge(GenotypeResults[,c("G10L.1min", "G10L.2max",
+                                 "MU05.1min", "MU05.2max",
+                                 "MU09.1min", "MU09.2max",
+                                 "MU10.1min", "MU10.2max",
+                                 "MU23.1min", "MU23.2max",
+                                 "MU50.1min", "MU50.2max",
+                                 "MU51.1min", "MU51.2max",
+                                 "MU59.1min", "MU59.2max")], 
+              metadata, 
+              by.x = "row.names", 
+              by.y = "index")
+  tt$confirmed_dead <- "No"
+  tt <- tt[,c(1,20,18,19,21,2:17)]
+  names(tt) <- header
+  tt$gender[is.na(tt$gender)] <- "Okänt"
+  if(filt == "No") {
+    tt
+  } else {
+    tt[rowSums(is.na(tt[7:22]))<5,]
+  }
+}
+
+CreateMatchInputDead <- function(GenotypeResults, filt = "No") {
+  header <- c("index", "date", "north", "east",
+              "gender", "confirmed_dead", "G10L_1",
+              "G10L_2", "MU05_1", "MU05_2", "MU09_1",
+              "MU09_2", "MU10_1", "MU10_2", "MU23_1",
+              "MU23_2", "MU50_1", "MU50_2", "MU51_1",
+              "MU51_2", "MU59_1", "MU59_2")
+  metadata <- read_excel("/home/thomkall/Develop/SSRqc/inst/extdata/DeadbearsRovbase.xlsx")
+  metadata <- metadata[,c(1,3:5)]
+  names(metadata) <- c("index", "north", "east", "date")
+  tt <- merge(GenotypeResults, metadata, by.x = "row.names", by.y = "index")
+  tt$confirmed_dead <- "Yes"
+  tt <- tt[,c("Row.names", "date", "north", "east", "consensus", "confirmed_dead", "G10L.1min",
+        "G10L.2max", "MU05.1min", "MU05.2max", "MU09.1min", "MU09.2max",
+        "MU10.1min", "MU10.2max", "MU23.1min", "MU23.2max", "MU50.1min",
+        "MU50.2max", "MU51.1min", "MU51.2max", "MU59.1min", "MU59.2max")]
+  names(tt) <- header
+  tt$gender[is.na(tt$gender)] <- "Okänt"
+  if(filt == "No") {
+    tt
+  } else {
+    tt[rowSums(is.na(tt[7:22]))<5,]
+  }
+}
+
+# CreateImportData <- function(fromMatchDB, RBData, fileImport, fileInd) {
+#    cols <- c("DNAID","Strekkode","RovbaseID",
+#              "ArtsID", "Provestatus", "KjonnID", "MetodeID",
+#              "VurderingID", "Kontrollstatus", "AnalysertAvID",
+#              "Merknad", "IndividID", "Individnavn", "ReferenseID",
+#              "Referensetekst")
+    
+    
+    
